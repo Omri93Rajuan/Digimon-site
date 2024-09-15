961c39eb2fda18343750f26cf7b5da26402f3bcc
Omri Rajuan <107933333+Omri93Rajuan@users.noreply.github.com>
2024-09-15 22:39:27 +0300

    --skip-tests

---
+++ Client/src/app/no-tests/no-tests.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-tests',
+  standalone: true,
+  imports: [],
+  templateUrl: './no-tests.component.html',
+  styleUrl: './no-tests.component.css'
+})
+export class NoTestsComponent {
+
+}
