b48ae98f61536736e624fc8ba99a30a9e5dc4435
Omri Rajuan <107933333+Omri93Rajuan@users.noreply.github.com>
2024-12-03 21:51:02 +0200

    Login and Register pages

--- client-react/tailwind.config.js
+++ client-react/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customGold: {
+          DEFAULT: "#EBC55A", // צבע ברירת מחדל
+          600: "#D4A947", // גרסה כהה יותר
+        },
+      },
+    },
   },
   plugins: [],
 };
