fd617a95d868e9cafe0ca0bc4951e43b8610d903
Omri Rajuan <107933333+Omri93Rajuan@users.noreply.github.com>
2024-12-16 06:40:51 +0200

    Change Fotter color

--- client-react/src/layout/footer/footer.tsx
+++ client-react/src/layout/footer/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-gradient-to-r from-yellow-200 via-customBlue-600 to-yellow-200 py-6 text-center shadow-xl">
+    <footer className="bg-gradient-to-r bg-customBlue-600 py-6 text-center shadow-xl">
       <p className="text-lg text-white opacity-80">
         © {new Date().getFullYear()} PIKA PIKA STORE. All rights reserved.
       </p>
