b5c05a393fdc2863c551be83894f9faafd7fa944
Omri Rajuan <107933333+Omri93Rajuan@users.noreply.github.com>
2024-12-19 17:59:03 +0200

    Remove commet on the code

--- client-react/src/providers/AuthProvider.tsx
+++ client-react/src/providers/AuthProvider.tsx
@@ -25,19 +25,14 @@ interface AuthContextType {
 export const AuthContext = createContext<AuthContextType | null>(null);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const { POST, data } = useFetch();
-
+  const { POST } = useFetch();
   const [user, setUser] = useState<IUser | null>(null);
-
   const [error, setError] = useState<string | null>(null);
 
-  // -----------LOGIN-----------
   const login = async (userFromClient: UserDto): Promise<boolean> => {
     try {
       const userData = await POST("auth/login", userFromClient);
       setUser(userData.foundUser);
-      console.log(userData.foundUser);
-
       return true;
     } catch (error) {
       setError("Login failed. Please try again.");
@@ -47,16 +42,11 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 
   const logout = async (): Promise<void> => {
     try {
-      // לא צריך לנהל את ה-response ישירות כי ה-hook כבר עושה זאת
-      const response = await POST("auth/logout", {}); // כאן אנחנו שולחים אובייקט ריק כיוון שאין צורך בנתונים במהלך logout
-
-      // אם התגובה לא בסדר, נסמן את השגיאה
+      const response = await POST("auth/logout", {});
       if (!response) {
         console.error("Logout failed, no response.");
         return;
       }
-
-      // במקרה של הצלחה, מאפסים את המשתמש
       setUser(null);
     } catch (error) {
       console.error("An unexpected error occurred during logout:", error);
