aa09c97d7da49176b0d25e14fcef0dd82a30bfe8
Omri Rajuan <107933333+Omri93Rajuan@users.noreply.github.com>
2024-12-24 00:57:06 +0200

    fix flex on storePage

--- client-react/src/store/StorePage.tsx
+++ client-react/src/store/StorePage.tsx
@@ -7,7 +7,7 @@ import CartSummarySidebar from "./components/CartSummarySidebar";
 import PageHeader from "../components/PageHeader";
 import { useAuth } from "../hooks/useAuth";
 
-export default function StorePage() {
+const StorePage = () => {
   const [productList, setProductList] = useState<IProduct[]>([]);
   const [selectedProduct, setSelectedProduct] = useState<IProduct | null>(null);
 
@@ -43,33 +43,39 @@ export default function StorePage() {
   return (
     <>
       <PageHeader title="The Store" />
-      <div className="p-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div className="p-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
         {productList.map((product) => (
           <div
             key={product._id}
             onClick={() => openProductModal(product)}
-            className="cursor-pointer border border-gray-200 rounded-lg shadow-lg transition duration-200 hover:shadow-xl flex flex-col"
+            className="cursor-pointer border border-gray-200 rounded-lg shadow-lg transition duration-200 hover:shadow-xl flex flex-col bg-white"
           >
             <img
               src={product.images[0]}
               alt={product.name}
-              className="w-full h-32 object-cover rounded-t-lg"
+              className="w-full h-40 object-cover rounded-t-lg"
             />
             <div className="flex-grow p-4 flex flex-col justify-between">
               <div>
-                <h1 className="text-xl font-semibold text-gray-800">
+                <h1 className="text-xl font-bold text-gray-800">
                   {product.name}
                 </h1>
-                <p className="text-sm text-gray-600">{product.description}</p>
-                <p className="text-xs text-gray-500">{product.category.name}</p>
-              </div>
-              <div className="mt-2">
-                <p className="text-lg font-bold text-green-600">
-                  ${product.price}
+                <p className="text-sm text-gray-600 mt-2">
+                  {product.description}
                 </p>
+              </div>
+              <div className="mt-4">
+                <div className="mt-4 flex justify-around">
+                  <p className="text-xs text-gray-500 mt-1">
+                    {product.category.name}
+                  </p>
+                  <p className="text-lg font-semibold text-green-600">
+                    ${product.price.toFixed(2)}
+                  </p>
+                </div>
                 <button
                   onClick={(e) => handleAddToCart(product, e)}
-                  className="mt-2 w-full bg-customBlue-600 text-white font-semibold py-1 px-2 rounded-lg hover:bg-customPink-600 transition duration-200 "
+                  className="mt-2 w-full bg-customBlue-600 text-white font-semibold py-2 px-4 rounded-lg hover:bg-customPink-600 transition duration-200"
                 >
                   Add to Cart
                 </button>
@@ -84,4 +90,6 @@ export default function StorePage() {
       <CartSummarySidebar />
     </>
   );
-}
+};
+
+export default StorePage;
