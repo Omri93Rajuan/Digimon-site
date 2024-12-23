4aa4b0b14a9564f737cd19a3c10e17680a07eac3
Omri Rajuan <107933333+Omri93Rajuan@users.noreply.github.com>
2024-12-24 01:04:08 +0200

    Update cartPage with the producet comp

--- client-react/src/store/CartPage.tsx
+++ client-react/src/store/CartPage.tsx
@@ -1,18 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useCart } from "../providers/CartProvider";
 import { useNavigate } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import PageHeader from "../components/PageHeader";
+import ProductModal from "../components/ProductModel";
+import { IProduct } from "../types/product.interface";
 
-export default function CartPage() {
+const CartPage = () => {
   const { cart, clearCart, setCartCount, updateQuantity, removeFromCart } =
     useCart();
   const { POST } = useFetch();
+  const [selectedProduct, setSelectedProduct] = useState<IProduct | null>(null);
   const navigate = useNavigate();
 
   useEffect(() => {
     setCartCount(0);
-  }, []);
+  }, [setCartCount]);
 
   const handleCheckout = async () => {
     try {
@@ -54,51 +57,61 @@ export default function CartPage() {
     removeFromCart(productId);
   };
 
+  const closeProductModal = () => setSelectedProduct(null);
+
+  const truncateText = (text: string, maxWords: number) => {
+    const words = text.split(" ");
+    if (words.length > maxWords) {
+      return words.slice(0, maxWords).join(" ") + "...";
+    }
+    return text;
+  };
+
   return (
     <div className="max-w-4xl mx-auto px-4 py-6">
-      <PageHeader title="Cart Page" />
-
+      <PageHeader title="Your Cart" />
       {cart.products.length === 0 ? (
         <div className="text-center text-xl text-gray-500">
           Your cart is empty.
         </div>
       ) : (
-        <div className="bg-white shadow-sm rounded-lg p-6">
-          <ul className="space-y-4">
+        <div className="bg-white shadow-lg rounded-lg p-6">
+          <ul className="space-y-6">
             {cart.products.map((item) => (
               <li
                 key={item.product._id}
                 className="flex justify-between items-center border-b pb-4"
               >
-                <div className="flex items-center space-x-4">
+                <div
+                  className="flex items-center space-x-4 cursor-pointer"
+                  onClick={() => setSelectedProduct(item.product)}
+                >
                   <img
                     src={item.product.images[0]}
                     alt={item.product.name}
-                    className="w-16 h-16 object-cover rounded-md"
+                    className="w-20 h-20 object-cover rounded-md"
                   />
-                  <span className="text-lg font-semibold text-gray-700">
-                    {item.product.name}
+                  <span className="text-lg font-bold text-gray-800">
+                    {truncateText(item.product.name, 5)}
                   </span>
                 </div>
                 <div className="flex items-center space-x-4">
                   <div className="flex items-center space-x-2">
                     <button
                       onClick={() => handleDecreaseQuantity(item.product._id!)}
-                      className="text-gray-600 text-lg font-semibold"
+                      className="text-gray-600 text-lg font-semibold border border-gray-200 rounded-full w-8 h-8 flex items-center justify-center"
                     >
                       -
                     </button>
-                    <span className="text-gray-500">
-                      Quantity: {item.quantity}
-                    </span>
+                    <span className="text-gray-700">{item.quantity}</span>
                     <button
                       onClick={() => handleIncreaseQuantity(item.product._id!)}
-                      className="text-gray-600 text-lg font-semibold"
+                      className="text-gray-600 text-lg font-semibold border border-gray-200 rounded-full w-8 h-8 flex items-center justify-center"
                     >
                       +
                     </button>
                   </div>
-                  <span className="text-xl font-semibold text-gray-900">
+                  <span className="text-lg font-semibold text-gray-900">
                     ${(item.product.price * item.quantity).toFixed(2)}
                   </span>
                   <button
@@ -114,11 +127,11 @@ export default function CartPage() {
           <div className="mt-6 flex justify-between items-center">
             <button
               onClick={clearCart}
-              className="bg-red-600 text-white px-6 py-2 rounded-full hover:bg-red-700"
+              className="bg-red-600 text-white px-6 py-2 rounded-full hover:bg-red-700 transition duration-200"
             >
               Clear Cart
             </button>
-            <div className="text-xl font-semibold text-gray-900">
+            <div className="text-2xl font-semibold text-gray-900">
               Total: $
               {cart.products
                 .reduce(
@@ -128,17 +141,21 @@ export default function CartPage() {
                 .toFixed(2)}
             </div>
           </div>
-
-          <div className="mt-4">
+          <div className="mt-6">
             <button
               onClick={handleCheckout}
-              className="w-full bg-blue-500 text-white text-lg font-semibold py-2 rounded-full hover:bg-blue-400"
+              className="w-full bg-customBlue-600 text-white text-lg font-semibold py-3 rounded-full hover:bg-customPink-600 transition duration-200"
             >
               Proceed to Checkout
             </button>
           </div>
         </div>
       )}
+      {selectedProduct && (
+        <ProductModal product={selectedProduct} onClose={closeProductModal} />
+      )}
     </div>
   );
-}
+};
+
+export default CartPage;
